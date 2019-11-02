be06deca4cdc3fc1b772b4cba8b931cf39c2c39b
Marcos Vinicius <viniciusmr1499@gmail.com>
2019-11-02 16:51:56 -0200

    page produto 99%

--- public/js/efect-field.js
+++ public/js/efect-field.js
@@ -1,3 +1,4 @@
+// EFEITO PLACEHOLDER - PÁGINA DE LOGIN
 $(document).ready(function(){
     $('.txtb input').on('focus',function(){
         $(this).addClass('focus')
@@ -10,12 +11,13 @@ $(document).ready(function(){
 })
 
 $(document).ready(function(){
-    $('.field__register input').on('focus',function(){
+    $('.r-info input').on('focus',function(){
         $(this).addClass('focus')
     })
     
-    $('.field__register input').on('blur',function(){
+    $('.r-info input').on('blur',function(){
         if($(this).val() == '')
         $(this).removeClass('focus')
     })
 })
+
